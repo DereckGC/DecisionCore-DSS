a8c730757b9d8cd1b9af0fb68b686483bbb8ad05
Dereck Gutiérrez Cordero <148293894+DereckGC@users.noreply.github.com>
2026-06-10 13:01:20 -0600

    correcciones en el modulo de analisis de desicion

--- features/desicion_analysis/logic/poe.py
+++ features/desicion_analysis/logic/poe.py
@@ -2,33 +2,22 @@ import streamlit as st
 import pandas as pd
 
 def calculate_poe(decision_df):
-    
     df_loss = calculate_loss_by_market(decision_df)
 
-    prob_markets = st.session_state.get("prob_markets", [])
-    if len(prob_markets) != len(df_loss.columns):
-        prob_markets = [1 / len(df_loss.columns)] * len(df_loss.columns)
+    poe_max = df_loss.max(axis=1)
 
-    df_weighted_losses = df_loss.multiply(prob_markets, axis=1)
-    
-    poe_sums = df_weighted_losses.sum(axis=1)
-
-    best_value = poe_sums.min()
-    best_alternatives = poe_sums[poe_sums == best_value].index.tolist()
+    best_value = poe_max.min()
+    best_alternatives = poe_max[poe_max == best_value].index.tolist()
 
     return {
-        "poe_results": poe_sums,
+        "poe_results": poe_max,
         "best_value": best_value,
         "best_alternatives": best_alternatives,
-        "loss_matrix": df_loss 
+        "loss_matrix": df_loss
     }
 
 def calculate_loss_by_market(decision_df):
     df_calc = decision_df.set_index("Alternatives")
     df_calc = df_calc.apply(pd.to_numeric, errors='coerce').fillna(0)
-
-    df_max_value_by_market = df_calc.max(axis=0)
-
-    df_losses = df_max_value_by_market - df_calc
-
+    df_losses = df_calc.max(axis=0) - df_calc
     return df_losses
